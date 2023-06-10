f6f28963f7f2e3fa7866bb2dc5ec30570278b932
namnpse <nam444479@gmail.com>
2023-06-11 01:57:07 +0700

    [test] add api tests using mock server

---
+++ app/src/main/java/com/namnp/newsapp/data/data_source/NewsLocalDataSource.kt
@@ -0,0 +1,10 @@
+package com.namnp.newsapp.data.data_source
+
+import com.namnp.newsapp.data.model.Article
+import kotlinx.coroutines.flow.Flow
+
+interface NewsLocalDataSource {
+    suspend fun saveArticleToDB(article: Article)
+    fun getSavedArticles(): Flow<List<Article>>
+    suspend fun deleteArticlesFromDB(article: Article)
+}
