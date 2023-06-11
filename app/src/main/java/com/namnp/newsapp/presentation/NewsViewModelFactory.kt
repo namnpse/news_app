d06386827845997590ca9cc8de813dcdfd836c4b
namnpse <nam444479@gmail.com>
2023-06-11 17:21:23 +0700

    [presentation] add navigation, bottom nav, fragments

--- app/src/main/java/com/namnp/newsapp/presentation/NewsViewModelFactory.kt
+++ app/src/main/java/com/namnp/newsapp/presentation/NewsViewModelFactory.kt
@@ -6,9 +6,9 @@ import androidx.lifecycle.ViewModelProvider
 import com.namnp.newsapp.domain.usecase.*
 
 class NewsViewModelFactory(
-    private val app:Application,
+    private val app: Application,
     private val getNewsHeadlinesUseCase: GetNewsHeadlinesUseCase,
-):ViewModelProvider.Factory {
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(
             app,
